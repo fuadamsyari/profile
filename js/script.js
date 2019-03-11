aadbe8e605ebcb5e2a022c62ddcbc2c6f91c70a4
fuadamsyari <48084093+fuadamsyari@users.noreply.github.com>
2019-03-11 08:49:02 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// even pada saat di klik
+$('.page-scrol').on('click', function(){
+
+	// abil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+
+	
+
+	
+
+});
