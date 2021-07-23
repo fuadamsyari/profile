f21ea37820404a8335a47e16d1f054d0b6a131e0
Ahmad Fuad Amsyari <48084093+fuadamsyari@users.noreply.github.com>
2021-07-23 19:46:07 +0700

    udate js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // even pada saat di klik
-$('.page-scrol').on('click', function(){
-
+$('.page-scrol').on('click', function(e){
+	e.preventDefault();
 	// abil isi href
 	var tujuan = $(this).attr('href');
 	// tangkap elemen ybs
 	var elemenTujuan = $(tujuan);
-
 	
+	var pos = elemenTujuan.offset().top -50;
+	
+	$('body, html'),animate ({
+		scrollTop : pos;
+	
+	})
 
 	
 
