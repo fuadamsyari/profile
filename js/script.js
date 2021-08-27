16dce7e205ba7175685110d0c19b408f038006ad
Ahmad Fuad Amsyari <ahmadfuadamsyari@gmail.com>
2021-08-27 15:04:54 +0700

    kembali ke awal

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// even pada saat di klik
+$('.page-scroll').on('click', function(e){
+	e.preventDefault();
+	// abil isi href
+	var tujuan = $(this).attr('href');
+	// tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
+	
+	var pos = elemenTujuan.offset().top -50;
+	
+	$('body, html').animate({
+		scrollTop : pos
+	});
+
+	
+
+})
